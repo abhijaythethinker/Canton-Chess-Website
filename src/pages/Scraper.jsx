c5159f0616a06047f95602179252951c42dbbabb
Abhijay Das <abhijayd@umich.edu>
2025-10-23 21:57:50 -0400

    Removed october registration

--- src/pages/Scraper.jsx
+++ src/pages/Scraper.jsx
@@ -215,11 +215,11 @@ function Scraper() {
                 </div>
 
                 <div className="w-full flex justify-around mb-4">
-                    <button
+                    {/* <button
                         onClick={() => setTmDate('Oct 25')}
                         className="py-3 px-8 rounded-lg bg-green-700 text-white border-2 border-green-700 hover:bg-white hover:text-green-700 font-semibold transition-colors duration-400">
                         Oct 25
-                    </button>
+                    </button> */}
                     <button
                         onClick={() => setTmDate('Nov 15')}
                         className="py-3 px-8 rounded-lg bg-green-700 text-white border-2 border-green-700 hover:bg-white hover:text-green-700 font-semibold transition-colors duration-400">
